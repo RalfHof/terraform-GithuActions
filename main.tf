5b741479ae854a7856dafedd4ff98b1bd15695bf
RalfHof <email adresse>
2024-11-14 12:18:43 +0100

    neu hinzugefügt

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "meineErsteInstanz" {
   ami           = "ami-0eddb4a4e7d846d6f"
   instance_type = "t2.micro"
   tags = {
-    Name = "dasisteingeänderterName"
+    Name = "DasIstDerWorkflow"
   }
 }
