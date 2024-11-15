6e780a44b3816f2b2d0e4d20d290743e3bde1940
RalfHof <email adresse>
2024-11-15 09:52:05 +0100

    Mein Github Actions

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "meineErsteInstanz" {
   ami           = "ami-0eddb4a4e7d846d6f"
   instance_type = "t2.micro"
   tags = {
-    Name = "Meine Github Actions"
+    Name = "Meine Github Actions ${count.index}"
   }
 }
 
