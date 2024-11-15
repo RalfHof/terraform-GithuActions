f80f84a6aefa7ef197f86de53e792877375017e1
RalfHof <email adresse>
2024-11-15 11:15:19 +0100

    Teständerung

--- main.tf
+++ main.tf
@@ -2,61 +2,44 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# Erste EC2-Instance
+# Erstellt EC2-Instance
 resource "aws_instance" "meineErsteInstanz" {
   count = 3
   ami           = "ami-0eddb4a4e7d846d6f"
   instance_type = "t2.micro"
   key_name = "terraformKey"
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
   tags = {
     Name = "Meine Github Actions ${count.index}"
   }
 }
 
+# Security Group erstellen (nur SSH-Zugriff)
+resource "aws_security_group" "ssh_access" {
+  name_prefix = "ssh-access"
+  description = "Allow SSH access"
+ 
 
+  ingress {
+    
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# VPC
-#resource "aws_vpc" "main_vpc" {
-#  cidr_block = "10.0.0.0/16"
-# tags = {
-#    Name = "main_vpc"
-#  }
-#}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# Subnet
-#resource "aws_subnet" "main_subnet" {
- # vpc_id            = aws_vpc.main_vpc.id
- # cidr_block        = "10.0.1.0/24"
- # availability_zone = "eu-central-1a"
-  #tags = {
-   # Name = "main_subnet"
-##  }
-#}
+  tags = {
+    Name = "SSH_Access"
+  }
+}
 
-# S3 Bucket
-#resource "aws_s3_bucket" "data_bucket" {
-  #bucket = "mein-s3-bucket"   # Einzigartiger Name für den Bucket
-  #tags = {
-  #  Name = "data_bucket"
- # }
-#}
-
-# RDS Instance
-#resource "aws_db_instance" "my_rds" {
-  #allocated_storage    = 20
-  #storage_type         = "gp2"
-  #engine               = "mysql"
-  #engine_version       = "5.7"   # Nutze eine unterstützte Version
-  #instance_class       = "db.t3.micro"  # Korrigierter Instance Type
-  #db_name              = "mydatabase"
-  #username             = "admin"
- # password             = "your_password" 
-  #publicly_accessible  = false
- # skip_final_snapshot  = true
- # tags = {
-  #  Name = "my_rds_database"
- # }
-#}
 
 output "instance_puplic_ips" {
   value = [aws_instance.meineErsteInstanz.*.public_ip]
